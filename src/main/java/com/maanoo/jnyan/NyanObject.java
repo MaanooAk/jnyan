13a1e9fed19d3c6cc868de0cd4de6b758ecb35ea
MaanooAk <akritasak@gmail.com>
2019-10-04 22:05:47 +0300

    Created a basic impl of the nyan object and builder

--- src/main/java/com/maanoo/jnyan/NyanObject.java
+++ src/main/java/com/maanoo/jnyan/NyanObject.java
@@ -3,6 +3,9 @@ package com.maanoo.jnyan;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class NyanObject {
@@ -24,7 +27,7 @@ public class NyanObject {
 
     public final ArrayList<NyanObject> parents;
     public final HashMap<String, NyanType> members;
-    // public final HashMap<String, NyanOperation> operations;
+    public final HashMap<String, NyanOperation> operations;
 
     public NyanObject(String name) {
         this(name, null);
@@ -36,32 +39,226 @@ public class NyanObject {
 
         parents = new ArrayList<>();
         members = new HashMap<>();
-        // operations = new HashMap<>();
+        operations = new HashMap<>();
     }
 
-    // ==========
+    private void reset() {
 
-    // TODO
+        parents.clear();
+        members.clear();
+        operations.clear();
 
-    public NyanObject() {
-        throw new RuntimeException();
+        mod++;
     }
+
+    // ===
 
     public boolean is(NyanObject o) {
-        throw new RuntimeException();
+        return this.equals(o) || hasParent(o);
     }
 
-    public void patch(Patch patch) {
-        throw new RuntimeException();
+    public boolean hasParent(NyanObject parent) {
+
+        if (parents.contains(parent)) return true;
+
+        for (final NyanObject i : parents) {
+            if (i.hasParent(parent)) return true;
+        }
+
+        return false;
     }
+
+    public boolean hasPatch(NyanObject.Patch patch) {
+
+        for (final NyanObject i : parents) {
+            if (i.patchjob == patch) return true;
+        }
+        for (final NyanObject i : parents) {
+            if (i.hasPatch(patch)) return true;
+        }
+
+        return false;
+    }
+
+    public List<NyanObject> parents() {
+        final ArrayList<NyanObject> l = new ArrayList<>();
+        // wrongly simulate bfs
+        for (final NyanObject i : parents)
+            l.add(i);
+        for (final NyanObject i : parents)
+            l.addAll(i.parents());
+
+        final HashSet<NyanObject> found = new HashSet<>();
+        for (final Iterator<NyanObject> iter = l.iterator(); iter.hasNext();) {
+            final NyanObject i = iter.next();
+            if (found.contains(i)) iter.remove();
+            else found.add(i);
+        }
+
+        return l;
+    }
+
+    // ===
+
+    public NyanType getMemberType(String member) {
+        final NyanType type = getOptionalMemberType(member);
+        if (type != null) return type;
+
+        throw new RuntimeException("" + name + " does not have member named " + member);
+    }
+
+    public NyanType getOptionalMemberType(String member) {
+        NyanType type = members.get(member);
+        if (type != null) return type;
+
+        for (final NyanObject i : parents) {
+            type = i.getOptionalMemberType(member);
+            if (type != null) return type;
+        }
+
+        return null;
+    }
+
+    // ===
+
+    public HashMap<String, ValueHolder> calc() {
+        final HashMap<String, NyanType> types = new HashMap<>();
+        final HashMap<String, ValueHolder> values = new HashMap<>();
+
+        // if (!patches.isEmpty()) throw new RuntimeException(this + " has patches");
+
+        final List<NyanObject> pars = parents();
+        pars.add(0, this);
+
+        System.out.println(pars);
+
+        for (final NyanObject i : pars) {
+            types.putAll(i.members);
+        }
+
+        for (final String i : types.keySet()) {
+            values.put(i, null);
+
+            final ArrayList<NyanOperation> l = new ArrayList<>();
+            final ArrayList<NyanOperation> ldefer = new ArrayList<>();
+
+            for (final NyanObject par : pars) {
+                if (par.operations.containsKey(i)) {
+                    final NyanOperation op = par.operations.get(i);
+
+                    if (!op.defer) {
+                        l.add(op);
+                    } else {
+                        ldefer.add(0, op);
+                    }
+                }
+            }
+
+            l.addAll(0, ldefer);
+            if (ldefer.size() > 0) System.out.println(">>>" + l);
+
+            int ip = 0;
+            while (l.get(ip).operation != NyanOperation.Operation.Assign) {
+                ip += 1;
+                if (ip == l.size()) throw new RuntimeException("In " + name + ", " + i + " is abstract");
+            }
+
+            final ValueHolder value = l.get(ip).value.value.copy();
+
+            System.out.println(i + " =\n  " + "" + " " + value);
+            while (--ip >= 0) {
+                final NyanOperation op = l.get(ip);
+                op.operation.perform(value, op.value.value);
+
+                System.out.println("  " + "" + " (" + op + ") " + value);
+            }
+
+            values.put(i, value);
+        }
+
+        return values;
+    }
+
+    // ===
+
+    void patch(Patch patch) {
+        if (patch.target != this) throw new RuntimeException();
+
+        final NyanObject old = new NyanObject(patch.name + "#" + name, patch);
+        old.parents.addAll(parents);
+        old.members.putAll(members);
+        old.operations.putAll(operations);
+        // TODO update dependencies
+        reset();
+
+        parents.add(old);
+        parents.addAll(patch.parentmods);
+        operations.putAll(patch.operations);
+    }
+
+    // ===
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String toStringExtended() {
+        final StringBuilder sb = new StringBuilder(name);
+
+        sb.append("(");
+        for (final NyanObject i : parents)
+            sb.append(i.name).append(", ");
+        if (parents.size() > 0) sb.delete(sb.length() - 2, sb.length());
+        sb.append("):");
+
+        return sb.toString() + " " + members + " " + operations;
+
+    }
+
+    // ==
 
     public static class Patch extends NyanObject {
 
         public final NyanObject target;
+        public final ArrayList<NyanObject> parentmods;
 
-        public Patch() {
-            throw new RuntimeException();
+        public Patch(String name, NyanObject target) {
+            super(name);
+            this.target = target;
+
+            parentmods = new ArrayList<>();
+
+            parents.add(target);
         }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public String toStringExtended() {
+            final StringBuilder sb = new StringBuilder(name);
+            sb.append("<").append(target.name).append(">");
+
+            if (!parentmods.isEmpty()) {
+                sb.append("[");
+                for (final NyanObject i : parents)
+                    sb.append(i.name).append(", ");
+                if (parents.size() > 0) sb.delete(sb.length() - 2, sb.length());
+                sb.append("]");
+            }
+
+            sb.append("(");
+            for (final NyanObject i : parents)
+                if (i != target) sb.append(i.name).append(", ");
+            if (parents.size() > 1) sb.delete(sb.length() - 2, sb.length());
+            sb.append("):");
+
+            return sb.toString() + " " + members + " " + operations;
+        }
+
     }
 
 }
---
+++ src/main/java/com/maanoo/jnyan/builders/NyanObjectBuilder.java
@@ -0,0 +1,186 @@
+
+package com.maanoo.jnyan.builders;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.maanoo.jnyan.Database;
+import com.maanoo.jnyan.NyanObject;
+import com.maanoo.jnyan.Token;
+import com.maanoo.jnyan.TokenIter;
+
+
+public class NyanObjectBuilder implements Builder<NyanObject> {
+
+    public final String name;
+    public final ArrayList<String> parents;
+
+    public final String target;
+    public final ArrayList<String> parentmods;
+
+    public final HashMap<String, NyanTypeBuilder> members;
+    public final HashMap<String, NyanOperationBuilder> operations;
+
+    public final HashMap<String, NyanObjectBuilder> children;
+
+    public NyanObjectBuilder(TokenIter iter, String namespace) {
+        name = (namespace != null ? namespace + "." : "") + iter.next().text;
+
+        parents = new ArrayList<>();
+        members = new HashMap<>();
+        operations = new HashMap<>();
+        children = new HashMap<>();
+        parentmods = new ArrayList<>();
+
+        if (iter.peek(0).text.equals("<")) {
+            iter.consume(Token.Type.Keyword, "<");
+
+            target = iter.next().text;
+            iter.consume(Token.Type.Keyword, ">");
+        } else {
+            target = null;
+        }
+
+        if (target != null && iter.peek(0).text.equals("[")) {
+            iter.consume(Token.Type.Keyword, "[");
+            while (!iter.peek(0).text.equals("[")) {
+
+                parentmods.add(iter.peek(0).text);
+                parents.add(iter.peek(0).text);
+                iter.skip(1);
+
+                if (iter.peek(0).text.equals("]")) {
+                    break;
+                } else {
+                    iter.consume(Token.Type.Keyword, ",");
+                }
+            }
+            iter.consume(Token.Type.Keyword, "]");
+        }
+
+        iter.consume(Token.Type.Keyword, "(");
+        while (!iter.peek(0).text.equals(")")) {
+
+            parents.add(iter.next().text);
+
+            if (iter.peek(0).text.equals(")")) {
+                break;
+            } else {
+                iter.consume(Token.Type.Keyword, ",");
+            }
+        }
+        iter.consume(Token.Type.Keyword, ")");
+        iter.consume(Token.Type.Keyword, ":");
+        iter.consume(Token.Type.Newline);
+
+        // String indent = iter.next().text; //(Token.Type.Indent);
+
+        while (iter.peek(0).type == Token.Type.Indent) {
+            iter.skip(1);
+
+            if (iter.peek(0).type == Token.Type.Newline) {
+                iter.skip(1);
+                continue;
+            }
+
+            final String name = iter.peek(0).text;
+            final String op = iter.peek(1).text;
+
+            if (op.equals(":")) {
+                iter.skip(2);
+                members.put(name, new NyanTypeBuilder(iter));
+
+                if (iter.peek(0).type != Token.Type.Newline) {
+
+                    operations.put(name, new NyanOperationBuilder(iter));
+                }
+
+            } else if (op.equals("(") || op.equals("<")) {
+                children.put(name, new NyanObjectBuilder(iter, name));
+
+            } else {
+                iter.skip(1);
+                operations.put(name, new NyanOperationBuilder(iter));
+
+            }
+            iter.consume(Token.Type.Newline);
+
+        }
+
+    }
+
+    public Set<String> depens() {
+        final Set<String> s = new HashSet<>();
+        depens(s);
+        return s;
+    }
+
+    public void depens(Set<String> depens) {
+
+        depens.addAll(parents);
+
+        if (target != null) depens.add(target);
+        depens.addAll(parentmods);
+
+        for (final NyanTypeBuilder i : members.values()) {
+            i.depens(depens);
+        }
+
+        for (final NyanOperationBuilder i : operations.values()) {
+            i.depens(depens);
+        }
+
+        for (final NyanObjectBuilder i : children.values()) {
+            i.depens(depens);
+        }
+
+    }
+
+    public NyanObject build(Database database) {
+
+        final NyanObject o;
+
+        if (target == null) {
+            o = new NyanObject(name);
+
+            // add root object
+            if (parents.isEmpty()) o.parents.add(NyanObject.RootObject);
+
+        } else {
+            NyanObject.Patch p;
+            o = p = new NyanObject.Patch(name, database.get(target));
+
+            for (final String i : parentmods)
+                p.parentmods.add(database.get(i));
+
+            // add root object
+            o.parents.add(NyanObject.RootPatch);
+        }
+
+        for (final String i : parents)
+            o.parents.add(database.get(i));
+
+        for (final Map.Entry<String, NyanTypeBuilder> i : members.entrySet()) {
+            o.members.put(i.getKey(), i.getValue().build(database));
+        }
+
+        for (final Map.Entry<String, NyanOperationBuilder> i : operations.entrySet()) {
+
+            o.operations.put(i.getKey(), i.getValue().build(database, o.getMemberType(i.getKey())));
+        }
+
+        return o;
+    }
+
+    @Override
+    public String toString() {
+        return name + (target == null ? "" : " " + target) //
+                + (parentmods.isEmpty() ? "" : " " + parentmods) //
+                + " " + parents + " " + members + " " + operations //
+                + " << " + depens();
+    }
+
+}
