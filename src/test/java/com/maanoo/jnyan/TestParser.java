e0fa8cc8b13ccd633ff31b165e5c0f6c92ad6eef
MaanooAk <akritasak@gmail.com>
2019-10-05 18:31:09 +0300

    Fixed the parser test

--- src/test/java/com/maanoo/jnyan/TestParser.java
+++ src/test/java/com/maanoo/jnyan/TestParser.java
@@ -17,6 +17,8 @@ public class TestParser {
 
         {
             final ArrayList<Token> l = p.parse("hello people");
+            l.remove(0);
+            l.remove(l.size() - 1);
 
             assertEquals(2, l.size());
             assertEquals("hello", l.get(0).text);
@@ -26,12 +28,15 @@ public class TestParser {
         }
         {
             final ArrayList<Token> l = p.parse("Hello():\n    people: int = 10\n    compressed:text=\"123\"");
+            l.remove(0);
+            l.remove(l.size() - 1);
 
             assertEquals(18, l.size());
 
-            assertEquals("[Name(Hello), Keyword((), Keyword()), Keyword(:), Newline(;\n"
-                    + "), Indent(    ), Name(people), Keyword(:), Name(int), Keyword(=), Number(10), Newline(;\n"
-                    + "), Indent(    ), Name(compressed), Keyword(:), Name(text), Keyword(=), Text(123)]",
+            assertEquals(
+                    "[Name(Hello), Keyword((), Keyword()), Keyword(:), Newline(\n"
+                            + "), Indent(1), Name(people), Keyword(:), Name(int), Keyword(=), Number(10), Newline(\n"
+                            + "), Indent(1), Name(compressed), Keyword(:), Name(text), Keyword(=), Text(123)]",
                     l.toString());
         }
 
